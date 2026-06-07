dc97d4568ad2931218975e7f48f4ba79c57ca671
SadiaTijjani <tijja24s@mtholyoke.edu>
2026-06-07 02:25:43 -0400

    everything centered on page

--- src/Email.tsx
+++ src/Email.tsx
@@ -1,12 +1,35 @@
 function Email(){
     return (
-        <div>
+        <div
+        style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "90vh"
+            }}
+            >
+        
+        <div
+        style={{
+            width: "500px",
+            height: "500px",
+            padding: "20px",
+            borderRadius:"12px",
+            border: "1px solid lightgray"
+        }}
+        >
             <p>Work Email</p>
             {/*creating an input box to display the Email*/}
             <input
             type="email"
             placeholder="name@company.com"
+            style={{
+                width: "300px",
+                padding: "10px",
+                borderRadius: "8px"
+            }}
             />
+        </div>
         </div>
     );
 }

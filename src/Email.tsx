9cf5cc4b04e435a21a32541cfd106392d39c50be
SadiaTijjani <tijja24s@mtholyoke.edu>
2026-06-07 01:00:57 -0400

    email component implemented

---
+++ src/Email.tsx
@@ -0,0 +1,13 @@
+function Email(){
+    return (
+        <div>
+            <p>Work Email</p>
+            {/*creating an input box to display the Email*/}
+            <input
+            type="email"
+            placeholder="name@company.com"
+            />
+        </div>
+    );
+}
+export default Email;
